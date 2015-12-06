994c7e4979b701227160562828d1485163ecbd1c
Francis Aiello <fran.aiello@dev9.com>
2015-12-05 17:04:41 -0800

    Initial commit

---
+++ src/main/java/com/aiello/exercise/FirstNonRepeatedCharacter.java
@@ -0,0 +1,60 @@
+package com.aiello.exercise;
+
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+
+public class FirstNonRepeatedCharacter {
+    /**
+     * Method accepts a string argument and parses each character one by one
+     * searching for the first non repeated character in entire string.  This
+     * version takes two passes to determine non repeated character.
+     * @param str
+     * @return
+     */
+    public static Character findFirstNonRepeatedCharInString(String str) {
+        Map<Character, Integer> map = new LinkedHashMap<Character, Integer>();
+
+        Character firstNonRepeatingCharacter=null;
+
+        // Convert to character array and store each character and its
+        // number of occurrences in string
+        for (char c : str.toLowerCase().toCharArray()) {
+            if (map.get(c) == null) {
+                map.put(c, 1);
+            } else {
+                //Integer count = map.get(c);
+                map.put(c, map.get(c)+1);
+            }
+        }
+
+        // Iterate across each entry and return the first character that is non-repeating
+        for (Map.Entry<Character, Integer> entry : map.entrySet()) {
+            if (entry.getValue() == 1) {
+                firstNonRepeatingCharacter = entry.getKey();
+                break;
+            }
+        }
+
+        return firstNonRepeatingCharacter;
+    }
+
+    /**
+     * Method accepts a string argument and parses each character one by one
+     * searching for the first non repeated character in entire string.  This
+     * version performs the scan in one pass.
+     * @param str
+     * @return
+     */
+    public static Character findFirstNonRepeatedCharInStringImproved(String str) {
+        Character firstNonRepeatingCharacter=null;
+        for (int i = 0; i < str.length(); i++) {
+            char c = str.charAt(i);
+            if (str.indexOf(c) == str.lastIndexOf(c)) {
+                firstNonRepeatingCharacter=c;
+                break;
+            }
+        }
+        return firstNonRepeatingCharacter;
+    }
+}
---
+++ src/test/java/com/aiello/exercise/FirstNonRepeatedCharacterTest.java
@@ -0,0 +1,26 @@
+package com.aiello.exercise;
+
+import org.testng.annotations.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FirstNonRepeatedCharacterTest {
+    @Test
+    public void findFirstNonRepeatedCharInString() {
+        Character character = FirstNonRepeatedCharacter.findFirstNonRepeatedCharInString("stress");
+        assertThat(character).isEqualTo('t');
+
+        character = FirstNonRepeatedCharacter.findFirstNonRepeatedCharInString("teeter");
+        assertThat(character).isEqualTo('r');
+
+        character = FirstNonRepeatedCharacter.findFirstNonRepeatedCharInString("aftaftftftftrrrrvvvvvvnnnnnikgzghgh");
+        assertThat(character).isEqualTo('i');
+    }
+
+    @Test
+    public void findBlah() {
+        Character character = FirstNonRepeatedCharacter.findFirstNonRepeatedCharInStringImproved("teeter");
+        assertThat(character).isEqualTo('r');
+    }
+
+}
